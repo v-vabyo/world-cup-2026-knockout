1861e80f26b5da852a2ac8372a4831b4178e2e97
v-vabyo <vebyvabyo@gmail.com>
2026-07-03 02:34:06 +0700

    Change API endpoint to fetch global matches instead of strictly World Cup to allow testing live matches

--- api/matches.js
+++ api/matches.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
     // In production, the API Key should be set in Vercel Environment Variables
     const apiKey = process.env.FOOTBALL_API_KEY;
     
-    const apiRes = await fetch(`https://api.football-data.org/v4/competitions/WC/matches?t=${Date.now()}`, {
+    const apiRes = await fetch(`https://api.football-data.org/v4/matches?t=${Date.now()}`, {
       headers: {
         'X-Auth-Token': apiKey,
         'Cache-Control': 'no-cache',
